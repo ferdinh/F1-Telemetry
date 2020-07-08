bab1a7e26ab427e5f225f0eb8d40c59f52149744
Ferdin Handoko <9620862+ferdinh@users.noreply.github.com>
2020-07-08 18:31:33 +1200

    Create PacketMotionData.cs

---
+++ F12020Telemetry/Data/PacketMotionData.cs
@@ -0,0 +1,95 @@
+﻿using F12020Telemetry.Packet;
+
+namespace F12020Telemetry.Data
+{
+    internal struct PacketMotionData
+    {
+        public PacketHeader Header;
+
+        /// <summary>
+        /// Data for all cars on track
+        /// </summary>
+        public CarMotionData[] CarMotionData;
+
+        // Note: All wheel arrays have the following order:
+        // RL, RR, FL, FR
+
+        // Extra player car ONLY data
+
+        /// <summary>
+        /// The suspension position
+        /// </summary>
+        public float[] SuspensionPosition;
+
+        /// <summary>
+        /// The suspension velocity
+        /// </summary>
+        public float[] SuspensionVelocity;
+
+        /// <summary>
+        /// The suspension acceleration
+        /// </summary>
+        public float[] SuspensionAcceleration;
+
+        /// <summary>
+        /// Speed of each wheel
+        /// </summary>
+        public float[] WheelSpeed;
+
+        /// <summary>
+        /// Slip ratio for each wheel
+        /// </summary>
+        public float[] WheelSlip;
+
+        /// <summary>
+        /// Velocity in local space
+        /// </summary>
+        public float LocalVelocityX;
+
+        /// <summary>
+        /// Velocity in local space
+        /// </summary>
+        public float LocalVelocityY;
+
+        /// <summary>
+        /// Velocity in local space
+
+        /// </summary>
+        public float LocalVelocityZ;
+
+        /// <summary>
+        /// Angular velocity x-component
+        /// </summary>
+        public float AngularVelocityX;
+
+        /// <summary>
+        ///  Angular velocity y-component
+        /// </summary>
+        public float AngularVelocityY;
+
+        /// <summary>
+        /// Angular velocity z-component
+        /// </summary>
+        public float AngularVelocityZ;
+
+        /// <summary>
+        /// Angular velocity x-component
+        /// </summary>
+        public float AngularAccelerationX;
+
+        /// <summary>
+        /// Angular velocity y-component
+        /// </summary>
+        public float AngularAccelerationY;
+
+        /// <summary>
+        /// Angular velocity z-component
+        /// </summary>
+        public float AngularAccelerationZ;
+
+        /// <summary>
+        /// Current front wheels angle in radians
+        /// </summary>
+        public float FrontWheelsAngle;
+    }
+}
