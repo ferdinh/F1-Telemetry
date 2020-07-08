94353b88f18ef8aa6caa9d442e32db40e1babd78
Ferdin Handoko <9620862+ferdinh@users.noreply.github.com>
2020-07-08 14:08:00 +1200

    Initialise Project

---
+++ F12020Telemetry/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace F12020Telemetry
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+        }
+
+}
