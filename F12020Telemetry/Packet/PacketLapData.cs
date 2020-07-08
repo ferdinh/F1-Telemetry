8470e709b3445e064983f05a58f7b5535b86016c
Ferdin Handoko <9620862+ferdinh@users.noreply.github.com>
2020-07-08 17:19:24 +1200

    Create PacketLapData.cs

---
+++ F12020Telemetry/Packet/PacketLapData.cs
@@ -0,0 +1,14 @@
+﻿using F12020Telemetry.Data;
+
+namespace F12020Telemetry.Packet
+{
+    internal struct PacketLapData
+    {
+        public PacketHeader Header;
+
+        /// <summary>
+        ///  Lap data for all cars on track
+        /// </summary>
+        public LapData[] LapData;
+    };
+}
