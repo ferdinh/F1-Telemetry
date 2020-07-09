e790b868bd2254c8ee466feee7e774a3c838d701
Ferdin Handoko <9620862+ferdinh@users.noreply.github.com>
2020-07-09 14:52:52 +1200

    Add Packet Header Decoder

--- F12020Telemetry/Decode.cs
+++ F12020Telemetry/Decode.cs
@@ -10,5 +10,28 @@ namespace F12020Telemetry
         private static readonly int MaxNumberOfCarsOnTrack = 22;
         private static readonly int NumberOfTyres = 4;
         private static readonly int NumberOfWeatherSamples = 20;
+
+        /// <summary>
+        /// Decode header packet.
+        /// </summary>
+        /// <param name="reader">The reader.</param>
+        /// <returns>Packet Header</returns>
+        private static PacketHeader Header(BinaryReader reader)
+        {
+            var packetHeader = new PacketHeader();
+
+            packetHeader.PacketFormat = reader.ReadUInt16();
+            packetHeader.GameMajorVersion = reader.ReadByte();
+            packetHeader.GameMinorVersion = reader.ReadByte();
+            packetHeader.PacketVersion = reader.ReadByte();
+            packetHeader.PacketId = (PacketTypes)reader.ReadByte();
+            packetHeader.SessionUID = reader.ReadUInt64();
+            packetHeader.SessionTime = reader.ReadSingle();
+            packetHeader.FrameIdentifier = reader.ReadUInt32();
+            packetHeader.PlayerCarIndex = reader.ReadByte();
+            packetHeader.SecondaryPlayerCarIndex = reader.ReadByte();
+
+            return packetHeader;
+        }
     }
 }
