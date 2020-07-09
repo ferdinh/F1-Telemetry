9493ee27255f6da2c56e9fe62e380d91937a569b
Ferdin Handoko <9620862+ferdinh@users.noreply.github.com>
2020-07-09 14:52:35 +1200

    Add Fixed Numbers

--- F12020Telemetry/Decode.cs
+++ F12020Telemetry/Decode.cs
@@ -7,5 +7,8 @@ namespace F12020Telemetry
 {
     public static class Decode
     {
+        private static readonly int MaxNumberOfCarsOnTrack = 22;
+        private static readonly int NumberOfTyres = 4;
+        private static readonly int NumberOfWeatherSamples = 20;
     }
 }
