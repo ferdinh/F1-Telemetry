bb416ac5fa693a617e19b1a642219e58f7ab8a6e
Ferdin Handoko <9620862+ferdinh@users.noreply.github.com>
2020-07-11 17:15:11 +1200

    Create CarStatusData.cs

---
+++ F12020Telemetry/Data/CarStatusData.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace F12020Telemetry.Data
+{
+    public class CarStatusData
+    {
+        public byte TractionControl;          // 0 (off) - 2 (high)
+        public byte AntiLockBrakes;           // 0 (off) - 1 (on)
+        public byte FuelMix;                  // Fuel mix - 0 = lean, 1 = standard, 2 = rich, 3 = max
+        public byte FrontBrakeBias;           // Front brake bias (percentage)
+        public byte pitLimiterStatus;         // Pit limiter status - 0 = off, 1 = on
+        public float FuelInTank;              // Current fuel mass
+        public float FuelCapacity;            // Fuel capacity
+        public float FuelRemainingLaps;       // Fuel remaining in terms of laps (value on MFD)
+        public UInt16 MaxRPM;                 // Cars max RPM, point of rev limiter
+        public UInt16 IdleRPM;                // Cars idle RPM
+        public byte MaxGears;                 // Maximum number of gears
+        public byte DrsAllowed;               // 0 = not allowed, 1 = allowed, -1 = unknown
+
+        public UInt16 DrsActivationDistance;  // 0 = DRS not available, non-zero - DRS will be available
+                                              // in [X] metres
+
+        public byte[] TyresWear;              // Tyre wear percentage
+        public byte ActualTyreCompound;       // F1 Modern - 16 = C5, 17 = C4, 18 = C3, 19 = C2, 20 = C1
+                                              // 7 = inter, 8 = wet
+                                              // F1 Classic - 9 = dry, 10 = wet
+                                              // F2 – 11 = super soft, 12 = soft, 13 = medium, 14 = hard
+                                              // 15 = wet
+        public byte VisualTyreCompound;       // F1 visual (can be different from actual compound)
+                                              // 16 = soft, 17 = medium, 18 = hard, 7 = inter, 8 = wet
+                                              // F1 Classic – same as above
+                                              // F2 – same as above
+        public byte TyresAgeLaps;             // Age in laps of the current set of tyres
+
+        public byte[] TyresDamage;            // Tyre damage (percentage)
+        public byte FrontLeftWingDamage;      // Front left wing damage (percentage)
+        public byte FrontRightWingDamage;     // Front right wing damage (percentage)
+        public byte RearWingDamage;           // Rear wing damage (percentage)
+
+        public byte DrsFault;                 // Indicator for DRS fault, 0 = OK, 1 = fault
+
+        public byte EngineDamage;             // Engine damage (percentage)
+        public byte GearBoxDamage;            // Gear box damage (percentage)
+        public sbyte VehicleFiaFlags;         // -1 = invalid/unknown, 0 = none, 1 = green
+                                              // 2 = blue, 3 = yellow, 4 = red
+
+        public float ErsStoreEnergy;          // ERS energy store in Joules
+
+        public byte ErsDeployMode;            // ERS deployment mode, 0 = none, 1 = medium
+                                              // 2 = overtake, 3 = hotlap
+
+        public float ErsHarvestedThisLapMGUK; // ERS energy harvested this lap by MGU-K
+
+        public float ErsHarvestedThisLapMGUH; // ERS energy harvested this lap by MGU-H
+        public float ErsDeployedThisLap;      // ERS energy deployed this lap
+    }
+}
