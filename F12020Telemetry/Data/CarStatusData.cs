7a95357639e37868279e17c352bda67a1c269ac5
Ferdin Handoko <9620862+ferdinh@users.noreply.github.com>
2020-07-11 17:46:22 +1200

    Update Documentation

--- F12020Telemetry/Data/CarStatusData.cs
+++ F12020Telemetry/Data/CarStatusData.cs
@@ -1,57 +1,165 @@
-﻿using System;
-
-namespace F12020Telemetry.Data
+﻿namespace F12020Telemetry.Data
 {
     public class CarStatusData
     {
-        public byte TractionControl;          // 0 (off) - 2 (high)
-        public byte AntiLockBrakes;           // 0 (off) - 1 (on)
-        public byte FuelMix;                  // Fuel mix - 0 = lean, 1 = standard, 2 = rich, 3 = max
-        public byte FrontBrakeBias;           // Front brake bias (percentage)
-        public byte pitLimiterStatus;         // Pit limiter status - 0 = off, 1 = on
-        public float FuelInTank;              // Current fuel mass
-        public float FuelCapacity;            // Fuel capacity
-        public float FuelRemainingLaps;       // Fuel remaining in terms of laps (value on MFD)
-        public ushort MaxRPM;                 // Cars max RPM, point of rev limiter
-        public ushort IdleRPM;                // Cars idle RPM
-        public byte MaxGears;                 // Maximum number of gears
-        public byte DrsAllowed;               // 0 = not allowed, 1 = allowed, -1 = unknown
+        /// <summary>
+        /// The traction control [0 (off) - 2 (high)]
+        /// </summary>
+        public byte TractionControl;
 
-        public ushort DrsActivationDistance;  // 0 = DRS not available, non-zero - DRS will be available
-                                              // in [X] metres
+        /// <summary>
+        /// The anti lock brakes 0 [(off) - 1 (on)]
+        /// </summary>
+        public byte AntiLockBrakes;
 
-        public byte[] TyresWear;              // Tyre wear percentage
-        public byte ActualTyreCompound;       // F1 Modern - 16 = C5, 17 = C4, 18 = C3, 19 = C2, 20 = C1
-                                              // 7 = inter, 8 = wet
-                                              // F1 Classic - 9 = dry, 10 = wet
-                                              // F2 – 11 = super soft, 12 = soft, 13 = medium, 14 = hard
-                                              // 15 = wet
-        public byte VisualTyreCompound;       // F1 visual (can be different from actual compound)
-                                              // 16 = soft, 17 = medium, 18 = hard, 7 = inter, 8 = wet
-                                              // F1 Classic – same as above
-                                              // F2 – same as above
-        public byte TyresAgeLaps;             // Age in laps of the current set of tyres
+        /// <summary>
+        /// Fuel mix - 0 = lean, 1 = standard, 2 = rich, 3 = max
+        /// </summary>
+        public byte FuelMix;
 
-        public byte[] TyresDamage;            // Tyre damage (percentage)
-        public byte FrontLeftWingDamage;      // Front left wing damage (percentage)
-        public byte FrontRightWingDamage;     // Front right wing damage (percentage)
-        public byte RearWingDamage;           // Rear wing damage (percentage)
+        /// <summary>
+        /// Front brake bias (percentage)
+        /// </summary>
+        public byte FrontBrakeBias;
 
-        public byte DrsFault;                 // Indicator for DRS fault, 0 = OK, 1 = fault
+        /// <summary>
+        /// Pit limiter status - 0 = off, 1 = on
+        /// </summary>
+        public byte pitLimiterStatus;
 
-        public byte EngineDamage;             // Engine damage (percentage)
-        public byte GearBoxDamage;            // Gear box damage (percentage)
-        public sbyte VehicleFiaFlags;         // -1 = invalid/unknown, 0 = none, 1 = green
-                                              // 2 = blue, 3 = yellow, 4 = red
+        /// <summary>
+        /// Current fuel mass
+        /// </summary>
+        public float FuelInTank;
 
-        public float ErsStoreEnergy;          // ERS energy store in Joules
+        /// <summary>
+        /// Fuel capacity
+        /// </summary>
+        public float FuelCapacity;
 
-        public byte ErsDeployMode;            // ERS deployment mode, 0 = none, 1 = medium
-                                              // 2 = overtake, 3 = hotlap
+        /// <summary>
+        /// Fuel remaining in terms of laps (value on MFD)
+        /// </summary>
+        public float FuelRemainingLaps;
 
-        public float ErsHarvestedThisLapMGUK; // ERS energy harvested this lap by MGU-K
+        /// <summary>
+        /// Cars max RPM, point of rev limiter
+        /// </summary>
+        public ushort MaxRPM;
 
-        public float ErsHarvestedThisLapMGUH; // ERS energy harvested this lap by MGU-H
-        public float ErsDeployedThisLap;      // ERS energy deployed this lap
+        /// <summary>
+        /// Cars idle RPM
+        /// </summary>
+        public ushort IdleRPM;
+
+        /// <summary>
+        /// Maximum number of gears
+        /// </summary>
+        public byte MaxGears;
+
+        /// <summary>
+        /// 0 = not allowed, 1 = allowed, -1 = unknown
+        /// </summary>
+        public byte DrsAllowed;
+
+        /// <summary>
+        /// The DRS activation distance
+        /// 0 = DRS not available, non-zero - DRS will be available
+        /// in [X] metres
+        /// </summary>
+        public ushort DrsActivationDistance;
+
+        /// <summary>
+        /// Tyre wear percentage
+        /// </summary>
+        public byte[] TyresWear;
+
+        /// <summary>
+        /// The actual tyre compound
+        /// F1 Modern - 16 = C5, 17 = C4, 18 = C3, 19 = C2, 20 = C1, 7 = inter, 8 = wet
+        /// F1 Classic - 9 = dry, 10 = wet
+        /// F2 – 11 = super soft, 12 = soft, 13 = medium, 14 = hard, 15 = wet
+        /// </summary>
+        public byte ActualTyreCompound;
+
+        /// <summary>
+        /// F1 visual (can be different from actual compound)
+        /// 16 = soft, 17 = medium, 18 = hard, 7 = inter, 8 = wet
+        /// F1 Classic – same as above
+        /// F2 – same as above
+        /// </summary>
+        public byte VisualTyreCompound;
+
+        /// <summary>
+        /// Age in laps of the current set of tyres
+        /// </summary>
+        public byte TyresAgeLaps;
+
+        /// <summary>
+        /// Tyre damage (percentage)
+        /// </summary>
+        public byte[] TyresDamage;
+
+        /// <summary>
+        /// Front left wing damage (percentage)
+        /// </summary>
+        public byte FrontLeftWingDamage;
+
+        /// <summary>
+        ///  Front right wing damage (percentage)
+        /// </summary>
+        public byte FrontRightWingDamage;
+
+        /// <summary>
+        /// Rear wing damage (percentage)
+        /// </summary>
+        public byte RearWingDamage;
+
+        /// <summary>
+        /// Indicator for DRS fault, 0 = OK, 1 = fault
+        /// </summary>
+        public byte DrsFault;
+
+        /// <summary>
+        /// Engine damage (percentage)
+        /// </summary>
+        public byte EngineDamage;
+
+        /// <summary>
+        /// Gear box damage (percentage)
+        /// </summary>
+        public byte GearBoxDamage;
+
+        /// <summary>
+        /// The vehicle fia flags
+        /// -1 = invalid/unknown, 0 = none, 1 = green
+        /// 2 = blue, 3 = yellow, 4 = red
+        /// </summary>
+        public sbyte VehicleFiaFlags;
+
+        /// <summary>
+        /// T ERS energy store in Joules
+        /// </summary>
+        public float ErsStoreEnergy;
+
+        /// <summary>
+        /// ERS deployment mode, 0 = none, 1 = medium, 2 = overtake, 3 = hotlap
+        /// </summary>
+        public byte ErsDeployMode;
+
+        /// <summary>
+        /// ERS energy harvested this lap by MGU-K
+        /// </summary>
+        public float ErsHarvestedThisLapMGUK;
+
+        /// <summary>
+        /// ERS energy harvested this lap by MGU-H
+        /// </summary>
+        public float ErsHarvestedThisLapMGUH;
+
+        /// <summary>
+        /// ERS energy deployed this lap
+        /// </summary>
+        public float ErsDeployedThisLap;
     }
 }
