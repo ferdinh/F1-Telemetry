2e8da9c15082e6f9b69901006fcc52103d35fcab
Ferdin Handoko <9620862+ferdinh@users.noreply.github.com>
2020-07-11 17:15:52 +1200

    Create PacketCarStatusData.cs

---
+++ F12020Telemetry/Packet/PacketCarStatusData.cs
@@ -0,0 +1,20 @@
+﻿using F12020Telemetry.Data;
+
+namespace F12020Telemetry.Packet
+{
+    public class PacketCarStatusData : IPacket
+    {
+        public PacketHeader Header { get; set; }
+        public CarStatusData[] CarStatusData;
+
+        private PacketCarStatusData()
+        {
+        }
+
+        public PacketCarStatusData(PacketHeader packetHeader)
+        {
+            Header = packetHeader;
+            CarStatusData = new CarStatusData[Decode.MaxNumberOfCarsOnTrack];
+        }
+    }
+}
