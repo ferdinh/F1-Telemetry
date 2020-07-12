2ebd7b144801d9bf477cdc34b61c62c5da668499
Ferdin Handoko <9620862+ferdinh@users.noreply.github.com>
2020-07-12 13:14:16 +1200

    Create EnumExtensionsTests.cs

---
+++ F1Telemetry.Test/ExtensionTests/EnumExtensionsTests.cs
@@ -0,0 +1,44 @@
+using F12020Telemetry.Util.Extensions;
+using FluentAssertions;
+using System;
+using System.ComponentModel.DataAnnotations;
+using Xunit;
+
+namespace F1Telemetry.Test.ExtensionTests
+{
+    public class EnumExtensionsTests
+    {
+        private enum TestEnum
+        {
+            [Display(Name = "With Display Name")]
+            WithDisplayAttrName,
+            WithoutDisplayAttrName
+        }
+
+        [Fact]
+        public void GetDisplayName_Given_Enum_With_DisplayAttribute_Should_Return_DisplayName()
+        {
+            // Arrange
+            var expected = "With Display Name";
+
+            // Act
+            var actual = TestEnum.WithDisplayAttrName.GetDisplayName();
+
+            // Arrange
+            actual.Should().Be(expected);
+        }
+
+        [Fact]
+        public void GetDisplayName_Given_Enum_Without_DisplayAttribute_Should_Return_EnumToString()
+        {
+            // Arrange
+            var expected = "WithoutDisplayAttrName";
+
+            // Act
+            var actual = TestEnum.WithoutDisplayAttrName.GetDisplayName();
+
+            // Arrange
+            actual.Should().Be(expected);
+        }
+    }
+}
