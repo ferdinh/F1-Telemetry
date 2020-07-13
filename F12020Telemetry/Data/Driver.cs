32a0a9078be4e73db2121c19a29c7de051cc25de
Ferdin Handoko <9620862+ferdinh@users.noreply.github.com>
2020-07-13 18:31:55 +1200

    Create Driver.cs

---
+++ F12020Telemetry/Data/Driver.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace F12020Telemetry.Data
+{
+    /// <summary>
+    /// Contains telemetry and driver information.
+    /// </summary>
+    public class Driver
+    {
+        /// <summary>
+        /// Gets or sets the number of laps the driver had done.
+        /// </summary>
+        /// <value>
+        /// The number of laps.
+        /// </value>
+        public int NumberOfLaps { get; private set; } = 0;
+
+        public IList<LapData> LapData { get; } = new List<LapData>();
+        public IList<CarTelemetryData> CarTelemetryData { get; internal set; } = new List<CarTelemetryData>();
+
+        public CarTelemetryData PlayerCurrentTelemetry => CarTelemetryData.LastOrDefault();
+    }
+}
