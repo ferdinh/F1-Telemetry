666d3e9f3ca9df81111a145a9ff35beea36cd791
Ferdin Handoko <9620862+ferdinh@users.noreply.github.com>
2020-07-14 13:15:31 +1200

    Create PacketParticipantsData.cs

---
+++ F12020Telemetry/Packet/PacketParticipantsData.cs
@@ -0,0 +1,28 @@
+﻿using F12020Telemetry.Data;
+
+namespace F12020Telemetry.Packet
+{
+    public class PacketParticipantsData : IPacket
+    {
+        public PacketHeader Header { get; set; }
+
+        public byte NumActiveCars;
+        public ParticipantData[] Participants;
+
+        private PacketParticipantsData()
+        {
+        }
+
+        public PacketParticipantsData(PacketHeader packetHeader)
+        {
+            Header = packetHeader;
+
+            Participants = new ParticipantData[Decode.MaxNumberOfCarsOnTrack];
+
+            for (int i = 0; i < Decode.MaxNumberOfCarsOnTrack; i++)
+            {
+                Participants[i] = new ParticipantData();
+            }
+        }
+    }
+}
