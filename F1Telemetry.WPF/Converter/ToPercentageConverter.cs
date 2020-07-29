ef480d23aee64658af6a940aac719f854c49b68a
Ferdin Handoko <9620862+ferdinh@users.noreply.github.com>
2020-07-29 22:13:46 +1200

    Create ToPercentageConverter.cs

---
+++ F1Telemetry.WPF/Converter/ToPercentageConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace F1Telemetry.WPF.Converter
+{
+    public class ToPercentageConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return $"{value:P2}";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Double.Parse(value.ToString());
+        }
+    }
+}
