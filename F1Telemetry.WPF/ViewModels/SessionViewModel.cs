736b474385ee85f554a124656d8547f548b4ba5f
Ferdin Handoko <9620862+ferdinh@users.noreply.github.com>
2020-07-29 22:12:30 +1200

    Create SessionViewModel.cs

---
+++ F1Telemetry.WPF/ViewModels/SessionViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Text;
+
+namespace F1Telemetry.WPF.ViewModels
+{
+    public class SessionViewModel : INotifyPropertyChanged
+    {
+        public string SessionType { get; set; }
+        public ushort TrackLength { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
