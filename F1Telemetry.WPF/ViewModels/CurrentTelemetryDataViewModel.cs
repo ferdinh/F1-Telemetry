bb4a9044f34439e02435bda6dc30bf26305bda49
Ferdin Handoko <9620862+ferdinh@users.noreply.github.com>
2020-07-29 22:13:16 +1200

    Create CurrentTelemetryDataViewModel.cs

---
+++ F1Telemetry.WPF/ViewModels/CurrentTelemetryDataViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace F1Telemetry.WPF.ViewModels
+{
+    public class CurrentTelemetryDataViewModel : INotifyPropertyChanged
+    {
+        public float Brake { get; set; }
+        public float Throttle { get; set; }
+        public ushort EngineRPM { get; set; }
+        public ushort Speed { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
