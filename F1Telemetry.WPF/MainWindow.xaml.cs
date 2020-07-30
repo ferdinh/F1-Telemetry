6fc257f76ed22865746686fdba0790d54f5f1628
Ferdin Handoko <9620862+ferdinh@users.noreply.github.com>
2020-07-30 13:51:12 +1200

    Update Usings

--- F1Telemetry.WPF/MainWindow.xaml.cs
+++ F1Telemetry.WPF/MainWindow.xaml.cs
@@ -1,17 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using F12020Telemetry;
+using F12020Telemetry.Data;
+using F12020Telemetry.Util.Extensions;
+using F1Telemetry.WPF.ViewModels;
+using ScottPlot;
+using System;
 using System.Linq;
-using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Threading;
+using System.Drawing;
 
 namespace F1Telemetry.WPF
 {
