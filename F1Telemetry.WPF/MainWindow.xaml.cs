b989df840376b6a14c02d881d252280477aa1179
Ferdin Handoko <9620862+ferdinh@users.noreply.github.com>
2020-07-30 14:18:09 +1200

    Create Variables

--- F1Telemetry.WPF/MainWindow.xaml.cs
+++ F1Telemetry.WPF/MainWindow.xaml.cs
@@ -21,6 +21,16 @@ namespace F1Telemetry.WPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        private PlottableSignalXY BrakeGraph;
+        private double[] currentRenderPosition = new double[1] { 0 };
+        private double[] currentRenderValue = new double[1] { 1000 };
+        private PlottableSignalXY GearGraph;
+        private DispatcherTimer GraphRenderTimer = new DispatcherTimer();
+        private bool IsListening;
+        private CancellationTokenSource ListeningCancellationTokenSource;
+        private PlottableSignalXY SpeedGraph;
+        private PlottableSignalXY ThrottleGraph;
+
         public MainWindow()
         {
             InitializeComponent();
