800f270cd018371509657ca38972b514adb5eb1c
Ferdin Handoko <9620862+ferdinh@users.noreply.github.com>
2020-07-31 21:56:40 +1200

    Create CurrentLapDataModel.cs

---
+++ F1Telemetry.WPF/Model/CurrentLapDataModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace F1Telemetry.WPF.Model
+{
+    public class CurrentLapDataModel
+    {
+        // 5000 for one minute of data.
+        public double[] Speed = new double[25_000];
+
+        public double[] Distance = new double[25_000];
+        public double[] Gear = new double[25_000];
+
+        public double[] Throttle = new double[25_000];
+        public double[] Brake = new double[25_000];
+    }
+}
