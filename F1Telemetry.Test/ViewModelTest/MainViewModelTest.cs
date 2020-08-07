18105b9b8d73b00ac19aab35140bfc8d97923850
Ferdin Handoko <9620862+ferdinh@users.noreply.github.com>
2020-08-07 17:23:44 +1200

    Add TopmostCommand Test

---
+++ F1Telemetry.Test/ViewModelTest/MainViewModelTest.cs
@@ -0,0 +1,21 @@
+﻿using F1Telemetry.WPF.ViewModels;
+using FluentAssertions;
+using Xunit;
+
+namespace F1Telemetry.Test.ViewModelTest
+{
+    public class MainViewModelTest
+    {
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public void TopmostCommand_Should_Set_To_Given_Input(bool input)
+        {
+            var viewModel = new MainViewModel();
+
+            viewModel.SetTopmostCommand.Execute(input);
+
+            viewModel.IsTopmost.Should().Be(input, because: $"it is instructed to be set to {input}");
+        }
+    }
+}
