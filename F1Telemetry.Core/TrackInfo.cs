383b882868a049cc2d0e93d9894d7daae3f9e90c
Ferdin Handoko <9620862+ferdinh@users.noreply.github.com>
2020-08-31 15:26:59 +1200

    Add Core Components

---
+++ F1Telemetry.Core/TrackInfo.cs
@@ -0,0 +1,36 @@
+﻿namespace F1Telemetry.Core
+{
+    public static class TrackInfo
+    {
+        public static string[] TrackNames { get; } = new []
+        {
+            "Melbourne",
+            "Paul Ricard",
+            "Shanghai",
+            "Sakhir (Bahrain)",
+            "Catalunya",
+            "Monaco",
+            "Montreal",
+            "Silverstone",
+            "Hockenheim",
+            "Hungaroring",
+            "Spa",
+            "Monza",
+            "Singapore",
+            "Suzuka",
+            "Abu Dhabi",
+            "Texas",
+            "Brazil",
+            "Austria",
+            "Sochi",
+            "Mexico",
+            "Baku(Azerbaijan)",
+            "Sakhir Short",
+             "Silverstone Short",
+            "Texas Short",
+            "Suzuka Short",
+            "Hanoi",
+            "Zandvoort"
+        };
+    }
+}
