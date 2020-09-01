331929e6e84e2c8bf55b71640374088fed59f9d9
Ferdin Handoko <9620862+ferdinh@users.noreply.github.com>
2020-09-01 21:32:12 +1200

    Add Update()

--- F1Telemetry.WPF/Model/CarTyreTemperatureModel.cs
+++ F1Telemetry.WPF/Model/CarTyreTemperatureModel.cs
@@ -13,54 +13,44 @@ namespace F1Telemetry.WPF.Model
 
     public class TyreTemperature : INotifyPropertyChanged
     {
-        private byte min;
-        private byte max;
-        private byte current;
-
-        public byte Min
-        {
-            get { return min; }
-        }
-
-        public byte Max
-        {
-            get { return max; }
-        }
-
-        public byte Current
-        {
-            get
-            {
-                return current;
-            }
-            set
-            {
-                current = value;
-                OnPropertyChanged();
-
-                if (value > max)
-                {
-                    max = value;
-                    OnPropertyChanged(nameof(max));
-                }
-
-                if (min == 0)
-                {
-                    min = value;
-                }
-                else
-                {
-                    if (value < min)
-                    {
-                        min = value;
-                    }
-                }
-            }
-        }
+        public byte Min { get; private set; }
+        public byte Max { get; private set; }
+        public byte Current { get; private set; }
 
 #pragma warning disable 169, 67
+
         public event PropertyChangedEventHandler PropertyChanged;
+
 #pragma warning restore 169, 67
+
+        public void Update(byte temperature)
+        {
+            Current = temperature;
+            OnPropertyChanged(nameof(Current));
+
+            if(IsMax(temperature))
+            {
+                Max = temperature;
+                OnPropertyChanged(nameof(Max));
+            }
+
+            if(IsMin(temperature) || Min == 0)
+            {
+                Min = temperature;
+                OnPropertyChanged(nameof(Min));
+            }
+
+
+            bool IsMax(byte temperature)
+            {
+                return temperature > Max;
+            }
+
+            bool IsMin(byte temperature)
+            {
+                return temperature < Min;
+            }
+        }
 
         protected void OnPropertyChanged([CallerMemberName] string name = null)
         {
