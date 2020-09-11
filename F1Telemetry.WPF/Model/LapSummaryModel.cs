29aef20048f32a68140507f0481f08a8a028bdfb
Ferdin Handoko <9620862+ferdinh@users.noreply.github.com>
2020-09-11 23:56:17 +1200

    Create LapSummaryModel.cs

---
+++ F1Telemetry.WPF/Model/LapSummaryModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace F1Telemetry.WPF.Model
+{
+    public class LapSummaryModel
+    {
+        public int LapNumber { get; set; }
+        public float LapTime { get; set; }
+    }
+}
