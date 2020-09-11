ae9b56b4dfe90fea28c8e949c6c1d7efc0234fb0
Ferdin Handoko <9620862+ferdinh@users.noreply.github.com>
2020-09-11 15:11:56 +1200

    Create BaseViewModel.cs

---
+++ F1Telemetry.WPF/ViewModels/BaseViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+
+namespace F1Telemetry.WPF.ViewModels
+{
+    /// <summary>
+    /// Base class for View Model.
+    /// </summary>
+    /// <seealso cref="System.ComponentModel.INotifyPropertyChanged" />
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
