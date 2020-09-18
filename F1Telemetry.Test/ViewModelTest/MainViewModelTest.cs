c2ab4ef5fddc948179102698219fcf89f6205937
Ferdin Handoko <9620862+ferdinh@users.noreply.github.com>
2020-09-18 23:36:16 +1200

    Create ClearAllGraphCommandTest

--- F1Telemetry.Test/ViewModelTest/MainViewModelTest.cs
+++ F1Telemetry.Test/ViewModelTest/MainViewModelTest.cs
@@ -1,5 +1,7 @@
 ﻿using F1Telemetry.WPF.ViewModels;
 using FluentAssertions;
+using ScottPlot;
+using System;
 using Xunit;
 
 namespace F1Telemetry.Test.ViewModelTest
@@ -17,5 +19,81 @@ namespace F1Telemetry.Test.ViewModelTest
 
             viewModel.IsTopmost.Should().Be(input, because: $"it is instructed to be set to {input}");
         }
+
+        [Fact]
+        public void ClearAllGraphCommand_Cannot_Execute_When_There_Is_No_Data()
+        {
+            var viewModel = new MainViewModel();
+
+            var actual = viewModel.ClearAllGraphCommand.CanExecute(null);
+
+            actual.Should().BeFalse();
+        }
+
+        [Fact]
+        public void ClearAllGraphCommand_Cannot_Execute_When_There_Is_No_Checked_Lap_Data()
+        {
+            var viewModel = new MainViewModel();
+            viewModel.LapSummaries.Add(new WPF.Model.LapSummaryModel
+            {
+                IsChecked = false
+            });
+
+            var actual = viewModel.ClearAllGraphCommand.CanExecute(null);
+
+            actual.Should().BeFalse();
+        }
+
+
+        [Fact]
+        public void ClearAllGraphCommand_Can_Execute_When_There_Is_Checked_Lap_Data()
+        {
+            var viewModel = new MainViewModel();
+            viewModel.LapSummaries.Add(new WPF.Model.LapSummaryModel
+            {
+                IsChecked = true
+            });
+
+            var actual = viewModel.ClearAllGraphCommand.CanExecute(null);
+
+            actual.Should().BeTrue();
+        }
+
+        [UIFact]
+        public void ClearAllGraphCommand_Can_Execute_When_LiveTelemetryIsToggled()
+        {
+            var viewModel = new MainViewModel();
+
+            viewModel.SpeedGraphPlot = new WpfPlot();
+            viewModel.ThrottleGraphPlot = new WpfPlot();
+            viewModel.BrakeGraphPlot = new WpfPlot();
+            viewModel.GearGraphPlot = new WpfPlot();
+
+            viewModel.EnableLiveTelemetryCommand.Execute(true);
+
+            var actual = viewModel.ClearAllGraphCommand.CanExecute(null);
+
+            actual.Should().BeTrue();
+        }
+
+        [UIFact]
+        public void ClearAllGraphCommand_Can_Execute_When_LiveTelemetryIsToggledOn_Then_Off()
+        {
+            var viewModel = new MainViewModel();
+
+            viewModel.SpeedGraphPlot = new WpfPlot();
+            viewModel.ThrottleGraphPlot = new WpfPlot();
+            viewModel.BrakeGraphPlot = new WpfPlot();
+            viewModel.GearGraphPlot = new WpfPlot();
+
+            viewModel.EnableLiveTelemetryCommand.Execute(true);
+
+            viewModel.EnableLiveTelemetryCommand.Execute(false);
+
+            var actual = viewModel.ClearAllGraphCommand.CanExecute(null);
+
+            actual.Should().BeTrue();
+        }
+
     }
 }
