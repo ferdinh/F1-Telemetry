0f1113d1a44a56b20eb5c511cf676210474cc10d
Ferdin Handoko <9620862+ferdinh@users.noreply.github.com>
2020-10-09 15:13:57 +1300

    Create CarInfo.cs

---
+++ F1Telemetry.Core/CarInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace F1Telemetry.Core
+{
+    public static class CarInfo
+    {
+        public static F1 F1 { get; } = new F1();
+    }
+
+    public class F1
+    {
+        public float MaxDeployableERS { get; } = 4000000.0f;
+    }
+}
